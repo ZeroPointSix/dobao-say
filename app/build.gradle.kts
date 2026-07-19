fac04f9b74df32875cb12fb010ab5bb94a9922cc
ZeroPointSix <h15002710886@gmail.com>
2026-07-19 13:37:46 +0800

    feat: Android press-to-talk PoC

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+group = "com.zeropointsix.dobaosay"
+version = "0.2.0-SNAPSHOT"
+
+android {
+    namespace = "com.zeropointsix.dobaosay"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.zeropointsix.dobaosay"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2.0-poc"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+    }
+}
+
+dependencies {
+    implementation(project(":asr-core"))
+    implementation(project(":provider-doubao"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+}
